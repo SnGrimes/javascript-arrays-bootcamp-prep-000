1bb620c5be1a4c7b89cfa1fd7f7227b860130174
Shantia Grimes-Farmer <shantiagrimes@gmail.com>
2018-06-07 14:51:47 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, item) {
+  return [item, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, item) {
+  return array.unshift(item);
+}
+
+function addElementToEndOfArray(array, item) {
+ return [...array, item]; 
+}
+
+function destructivelyAddElementToEndOfArray(array, item) {
+  return array.push(item);
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.shift(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  
+}
